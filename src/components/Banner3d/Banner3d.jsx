e48a33e9bc26c4dac8b6d7fa3420a475b192fdad
Johel Roque <johel0rmr@gmail.com>
2024-11-27 22:24:25 -0500

    feat(css): mejora en los estilos del banner

--- src/components/Banner3d/Banner3d.jsx
+++ src/components/Banner3d/Banner3d.jsx
@@ -7,6 +7,7 @@ export function Banner3d() {
 }
 const Container =styled.div`
     width: 100%;
+    height: 192px;
     background-image: linear-gradient(270deg, rgb(255, 240, 179) 0%, rgb(255, 196, 0) 100%);
     background-size: cover;
     background-position: center center;
